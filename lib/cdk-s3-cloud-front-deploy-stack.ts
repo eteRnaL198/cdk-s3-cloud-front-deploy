d6ab862580151b98993784560aa5a24864594a85
nagaki ikuya <ikuya@i-nagaki.local>
2022-01-07 11:14:16 +0900

    バケット作成

--- lib/cdk-s3-cloud-front-deploy-stack.ts
+++ lib/cdk-s3-cloud-front-deploy-stack.ts
@@ -1,16 +1,27 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class CdkS3CloudFrontDeployStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketName: string = this.node.tryGetContext('s3').bucketName;
+    const bucket = new s3.Bucket(this, 'S3Bucket', {
+      bucketName: bucketName,
+      // Bucketへの直接アクセスを禁止
+      accessControl: s3.BucketAccessControl.PRIVATE,
+      // CDK Stack削除時にBucketも削除する
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkS3CloudFrontDeployQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+
+
   }
 }
